f2b33e9e1cb6277ce540b66d1587fa78eaf54e02
Kush <kuriaisac@gmail.com>
2024-10-11 08:04:26 +0300

    updates changes

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@
 
 from flask import request, session
 from flask_restful import Resource
+from werkzeug.security import check_password_hash
 
 from config import app, db, api
 from models import User
@@ -28,16 +29,39 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user = User.query.filter(User.id==session.get('user_id')).first()
+        if user:
+            return user.to_dict()
+        else:
+            return {"error": "Unauthorized"}, 204
+        
+        
+
 
 class Login(Resource):
-    pass
+    def post(self):
+        data = request.get_json()        
+        username = data['username']
+        password = data['password']
+        user = User.query.filter_by(username=username).first()
+        
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        
+        return {"message": "Invalid username or password"}, 400
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None
+        return {"message": "Unauthorized Content"}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
